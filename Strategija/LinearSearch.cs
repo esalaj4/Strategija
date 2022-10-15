b12021b807fba83b9b812be2589709e4a2207f06
Ena Salaj <86615856+esalaj4@users.noreply.github.com>
2022-10-15 17:45:42 +0200

    Add files via upload
    Napisati sučelje strategije za sortiranje polja realnih brojeva. Implementirati tri različita algoritma za
    sortiranje. Napisati klasu NumberSequence koja održava slijed realnih brojeva zadane veličine. Ona treba
    omogućiti sortiranje slijeda. Klasa koristi sučelje strategije i metodu za postavljanje željenog algoritma
    sortiranja

---
+++ Strategija/LinearSearch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPPOON_LV7
+{
+    class LinearSearch:SearchStrategy
+    {
+        public override void Search(double[] array,double number)
+        {
+            int j = 0;
+            for (int i = 0; i < array.Length;i++)
+            {
+                if (array[i] == number)
+                {
+                    j++;
+                    Console.WriteLine("Broj pronadjen");
+                    return;
+                }
+
+            }
+            if (j == 0)
+                Console.WriteLine("Broj nije pronadjen");
+
+        }
+    }
+}
