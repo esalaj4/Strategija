f294a4dbc048a2c3bdca52d956febb890d4ac31f
Ena Salaj <86615856+esalaj4@users.noreply.github.com>
2022-10-15 17:52:44 +0200

    Add files via upload

---
+++ Iterator/Iterator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPPOON_LV6_2
+{
+    class Iterator : IAbstractIterator
+    {
+        private Box box;
+        private int currentPosition;
+        public Iterator(Box box)
+        {
+            this.box = box;
+            currentPosition = 0;
+        }
+
+        public bool IsDone { get { return this.currentPosition >= box.Count; } }
+
+        public Product First (){ return this.box[0];  }
+
+        public Product Current{get{return this.box[currentPosition]; } }
+
+        public Product Next()
+        {
+            currentPosition++;
+            if(this.IsDone)
+            {
+                return null;
+            }
+            else
+            {
+                return this.box[currentPosition];
+            }
+        }
+
+
+
+
+    }
+}
---
+++ Iterator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RPPOON_LV6_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product("bijela cokolada", 21);
+            Product product2 = new Product("tamna cokolada", 21);
+            Product product3 = new Product("cokolada od jagode", 21);
+
+            Box box = new Box();
+            box.AddProduct(product1);
+            box.AddProduct(product2);
+            box.AddProduct(product3);
+
+            Iterator iterator = new Iterator(box);
+
+            Console.WriteLine(iterator.First().ToString());
+        }
+    }
+}
